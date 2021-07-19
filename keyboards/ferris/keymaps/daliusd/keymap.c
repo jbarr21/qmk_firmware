edd9a92983552c350d6937b5f74cf10ce26f7d64
Dalius Dobravolskas <daliusd@wix.com>
2021-07-19 18:54:38 +0300

    One more little change.

--- keyboards/ferris/keymaps/daliusd/keymap.c
+++ keyboards/ferris/keymaps/daliusd/keymap.c
@@ -45,6 +45,7 @@ enum ferris_tap_dances {
 #define HOME_SLS    MT(MOD_LALT, KC_SLSH)
 
 #define KGUI_W      LGUI(KC_W)
+#define KGUI_Q      LGUI(KC_Q)
 #define KGUI_TLD    LGUI(KC_TILDE)
 
 #define K_PRINT     (QK_LCTL | QK_LSFT | QK_LGUI | KC_4)
@@ -141,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QUICK] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
-     XXXXXXX ,KGUI_W  ,XXXXXXX ,XXXXXXX ,KGUI_TLD,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+     KGUI_Q  ,KGUI_W  ,XXXXXXX ,XXXXXXX ,KGUI_TLD,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
      KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC ,                          KC_CIRC ,KC_AMPR ,KC_ASTR ,KC_LPRN ,KC_RPRN ,
   //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
